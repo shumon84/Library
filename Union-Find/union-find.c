408ccee132bb9fcb3b44293739fa82d8aff35277
藤田　朱門 <fujitashumon@shumon.local>
2017-04-02 17:05:38 +0900

    fixes

---
+++ Union-Find/union-find.c
@@ -0,0 +1,38 @@
+int union_find_par[MAX_N];
+int union_find_rank[MAX_N];
+
+void init(int n)
+{
+  for(i=0;i<n;i++)
+    {
+      union_find_par[i]=i;
+      union_find_rank[i]=0;
+    }
+}
+
+int find(int x)
+{
+  if(union_find_par[x]==x)
+    return x;
+  else
+    return union_find_par[x]=find(union_find_par[x]);
+}
+
+void unite(int x,int y)
+{
+  x=find(x);
+  y=find(y);
+  if(x==y)
+    return x;
+  if(union_find_rank[x]<union_find_rank[y])
+    union_find_par[x]=y;
+  else
+    union_find_par[y]=x;
+  if(union_find_rank[x]==union_find_rank[y])
+    union_find_rank[x]++;
+}
+
+int same(int x,int y)
+{
+  return find(x)==find(y);
+}

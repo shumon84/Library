cf5cc2f1999efa25a4bd93c6d7bab09e2f334d06
藤田　朱門 <fujitashumon@shumon.local>
2017-07-14 11:26:23 +0900

    fixed

--- Bits/ctz.c
+++ Bits/ctz.c
@@ -1,4 +1,4 @@
-/* CTZ(Count Trailing Zero)最上位ビットから連続する0を求める */
+/* CTZ(Count Trailing Zero)最下位ビットから連続する0を求める */
 #define ctz(n) __builtin_ctz(n)	    /* unsigned int */
 #define ctzl(n) __builtin_ctzl(n)   /* unsigned long */
 #define ctzll(n) __builtin_ctzll(n) /* unsigned long long */
--- Elementary_Math/pascal.c
+++ Elementary_Math/pascal.c
@@ -1,8 +1,15 @@
 /* パスカルの三角形(二項定理) */
-
-int pascal(int n,int k)
+#define PASCAL_N 1000
+#define PASCAL_K 1000
+long long ps[PASCAL_N][PASCAL_K];
+long long  pascal(int n,int k)
 {
-    if(k==0||n==k)
-      return 1;
-    return pascal(n-1,k-1)+pascal(n-1,k);
+  if(ps[n][k]==0)
+    {
+      if(k==0||n==k)
+	return ps[n][k]=1;
+      return ps[n][k]=ps[n-1][k-1]+ps[n-1][k];
+    }
+  else
+    return ps[n][k];
 }
